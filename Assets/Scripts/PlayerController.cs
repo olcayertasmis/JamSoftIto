95ffe768a74f2979e90259b71808df6616a1bfa7
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-05-11 00:08:33 +0300

    Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,49 +1,83 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
     [Header("Compenents")]
     private Rigidbody2D _rb;
+    private Animator _animator;
 
     [Header("Değişkenler")]
     [SerializeField] private float speed;
-    private float horizontal, vertical;
-    
+    private float _horizontal, _vertical;
+
+    [Header("Other Objects")]
+    [SerializeField] private GameObject sleepPanel;
+    [SerializeField] private GameObject nightPanel;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     void Start()
     {
-        
     }
 
     private void FixedUpdate()
     {
-        Move();    
+        Move();
     }
 
     void Update()
     {
-
     }
 
     private void Move()
     {
-        horizontal = Input.GetAxis("Horizontal");
-        vertical = Input.GetAxis("Vertical");
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
 
-        _rb.velocity = new Vector2(horizontal * (speed * Time.deltaTime), vertical * (speed * Time.deltaTime));
+        _rb.velocity = new Vector2(_horizontal * (speed * Time.deltaTime), _vertical * (speed * Time.deltaTime));
 
-        if (horizontal > 0) Debug.Log(" sağ ");
+        _animator.SetFloat("Horizontal", _horizontal);
+        _animator.SetFloat("Vertical", _vertical);
+
+        /*if (horizontal > 0) Debug.Log(" sağ ");
         else Debug.Log("sol");
 
         if (vertical > 0) Debug.Log("yukarı");
-        else Debug.Log("aşağı");
-          
+        else Debug.Log("aşağı");*/
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Bed"))
+        {
+            Message();
+        }
+    }
+
+    private void Message()
+    {
+        sleepPanel.SetActive(true);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            transform.position = new Vector2(5f, 1.75f);
+            StartCoroutine(CloseEye());
+        }
+    }
+
+    private IEnumerator CloseEye()
+    {
+        nightPanel.SetActive(true);
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("Dream");
     }
 }
