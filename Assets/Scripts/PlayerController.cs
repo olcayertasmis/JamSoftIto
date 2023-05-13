2bc645c007bf8b58c0622ac79e263c589a761a8e
Olcay Ertaşmış <ertasolcay@hotmail.com>
2023-05-13 14:38:58 +0300

    Upgrade this game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,10 +14,19 @@ public class PlayerController : MonoBehaviour
     [Header("Değişkenler")]
     [SerializeField] private float speed;
     private float _horizontal, _vertical;
+    [SerializeField] private float jumpPower;
+    static bool _isEat;
+    static float _jumpBuff;
+    private bool _isFinish;
 
     [Header("Other Objects")]
     [SerializeField] private GameObject sleepPanel;
     [SerializeField] private GameObject nightPanel;
+    [SerializeField] private GameObject deathPanel;
+    [SerializeField] private GameObject eatPanel;
+    [SerializeField] private GameObject mustEatPanel;
+    [SerializeField] private GameObject alreadyAtePanel;
+    [SerializeField] private GameObject finishPanel;
 
     private void Awake()
     {
@@ -25,17 +34,9 @@ public class PlayerController : MonoBehaviour
         _animator = GetComponent<Animator>();
     }
 
-    void Start()
-    {
-    }
-
     private void FixedUpdate()
     {
         Move();
-    }
-
-    void Update()
-    {
     }
 
     private void Move()
@@ -43,43 +44,101 @@ public class PlayerController : MonoBehaviour
         _horizontal = Input.GetAxis("Horizontal");
         _vertical = Input.GetAxis("Vertical");
 
-        _rb.velocity = new Vector2(_horizontal * (speed * Time.deltaTime), _vertical * (speed * Time.deltaTime));
+        var activeSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        if (activeSceneIndex == 0)
+            _rb.velocity = new Vector2(_horizontal * (speed * Time.deltaTime), _vertical * (speed * Time.deltaTime));
+
+        else
+        {
+            _rb.velocity = new Vector2(_horizontal * (speed * Time.deltaTime), 0);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _rb.AddForce(Vector2.up * (jumpPower + _jumpBuff), ForceMode2D.Impulse);
+            }
+        }
 
         _animator.SetFloat("Horizontal", _horizontal);
         _animator.SetFloat("Vertical", _vertical);
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        if (collision.gameObject.CompareTag("Bed"))
+        if (other.gameObject.CompareTag("Finish"))
         {
-            Message();
+            finishPanel.SetActive(true);
         }
-        else sleepPanel.SetActive(false);
     }
 
-    /*private void OnCollisionExit2D(Collision2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Bed"))
+        if (collision.gameObject.CompareTag("Bed")) Message();
+        else if (collision.gameObject.CompareTag("Desk")) EatPanel();
+        else
         {
             sleepPanel.SetActive(false);
+            mustEatPanel.SetActive(false);
+            eatPanel.SetActive(false);
+            alreadyAtePanel.SetActive(false);
         }
-    }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Death"))
+        {
+            StartCoroutine(OpenHouseScene());
+        }
+    }
+
+    private void EatPanel()
+    {
+        if (!_isEat) eatPanel.SetActive(true);
+        else alreadyAtePanel.SetActive(true);
+    }
+
+    public void EatButton()
+    {
+        _jumpBuff += 100f;
+        _isEat = true;
+        eatPanel.SetActive(false);
+        alreadyAtePanel.SetActive(false);
+    }
 
     private void Message()
     {
         sleepPanel.SetActive(true);
+
         if (Input.GetKeyDown(KeyCode.E))
         {
-            transform.position = new Vector2(5f, 1.75f);
-            StartCoroutine(CloseEye());
+            if (!_isEat)
+            {
+                sleepPanel.SetActive(false);
+                mustEatPanel.SetActive(true);
+            }
+            else
+            {
+                transform.position = new Vector2(5f, 1.75f);
+                StartCoroutine(OpenDreamScene());
+            }
         }
     }
 
-    private IEnumerator CloseEye()
+    private IEnumerator OpenDreamScene()
     {
         nightPanel.SetActive(true);
+
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene("Dream");
+    }
+
+    private IEnumerator OpenHouseScene()
+    {
+        deathPanel.SetActive(true);
+        _isEat = false;
+
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene("House");
     }
 }
